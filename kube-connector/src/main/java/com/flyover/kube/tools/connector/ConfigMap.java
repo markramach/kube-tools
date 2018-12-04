48540536a777a02bb48a54eb88884a2d39c7cdf0
Allen Newton <allen.newton5@gmail.com>
2018-12-04 10:19:01 -0600

    added configmap to kube-connector

---
+++ kube-connector/src/main/java/com/flyover/kube/tools/connector/ConfigMap.java
@@ -0,0 +1,57 @@
+package com.flyover.kube.tools.connector;
+
+import com.flyover.kube.tools.connector.model.ConfigMapModel;
+import com.flyover.kube.tools.connector.model.KubeMetadataModel;
+
+import java.util.Map;
+
+public class ConfigMap {
+    private Kubernetes kube;
+    private ConfigMapModel model = new ConfigMapModel();
+
+    public KubeMetadataModel metadata() {
+        return this.model.getMetadata();
+    }
+
+    public ConfigMap(Kubernetes kube) {
+        this.kube = kube;
+    }
+
+    public Map<String, String> data() {
+        return this.model.getData();
+    }
+
+    public ConfigMap data(String key, String value) {
+        this.model.getData().put(key, value);
+        return this;
+    }
+
+    public String data(String key) {
+        return this.model.getData().get(key);
+    }
+
+    public ConfigMap find() {
+
+        this.model = kube.find(this.model);
+
+        if(this.model == null) {
+            return null;
+        }
+
+        return this;
+    }
+
+    public ConfigMap create() {
+
+        this.model = kube.create(this.model);
+
+        return this;
+    }
+
+    public ConfigMap replace() {
+
+        kube.delete(this.model);
+
+        return create();
+    }
+}
