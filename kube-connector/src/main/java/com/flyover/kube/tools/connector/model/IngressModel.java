e4ae0bba16c0bddcc8d4583b61277468555e71ae
markramach <mark.ramach@centurylink.com>
2019-01-04 11:22:31 -0800

    annotations part of checksum

--- kube-connector/src/main/java/com/flyover/kube/tools/connector/model/IngressModel.java
+++ kube-connector/src/main/java/com/flyover/kube/tools/connector/model/IngressModel.java
@@ -5,6 +5,8 @@ package com.flyover.kube.tools.connector.model;
 
 import java.security.MessageDigest;
 import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -36,9 +38,14 @@ public class IngressModel extends KubeModel {
 		
 		try {
 		
+			Map<String, String> annotations = new LinkedHashMap<>(getMetadata().getAnnotations());
+			// This value mutates as the spec mutates and should be ignored.
+			annotations.remove("com.flyover.checksum");
+			
 			ObjectMapper mapper = new ObjectMapper();
 			
 			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(mapper.writeValueAsBytes(annotations));
 			md.update(mapper.writeValueAsBytes(getSpec()));
 			
 			return new String(Base64.getEncoder().encodeToString(md.digest()));
